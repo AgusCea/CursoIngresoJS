f3482295f8a89599f04aa8b1d1b358b7bb931771
AgusCea <52534887+AgusCea@users.noreply.github.com>
2019-07-11 12:30:17 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,8 @@
 function mostrar()
 {
-
+  var precio;
+  var descuento;
+  var precioFinal;
+  precio = parseInt(prompt("Escribir el precio"));
+  descuento = parseInt(prompt("Escribir el valor de descuento"));
 }
