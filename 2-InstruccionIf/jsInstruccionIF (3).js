273885dc695e20ae5afdbd2ac409f61a68ec955b
AgusCea <52534887+AgusCea@users.noreply.github.com>
2019-07-11 12:29:06 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,10 @@
 function mostrar()
 {
-//tomo la edad  
+  var edad;
+  edad = document.getElementById("edad").value;
+  if(edad>18){alert("eres mayor de edad")}
+  else{alert("no sos mayor de edad, andá a jugar Minecraft")}
+//tomo la edad
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,9 @@
 function mostrar()
 {
-//tomo la edad  
+  var edad;
+  edad = document.getElementById("edad").value;
+  if(edad<13 || edad>17) {alert("no eres un adolescente")}
+//tomo la edad
 
 
 }//FIN DE LA FUNCIÓN
