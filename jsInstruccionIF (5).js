9c64175ce6eb48375ab5ffd8b84addfb3bac5de1
AgusCea <52534887+AgusCea@users.noreply.github.com>
2019-07-12 08:56:02 -0300

    Delete jsInstruccionIF (5).js

--- jsInstruccionIF (5).js
+++
@@ -1,9 +0,0 @@
-function mostrar()
-{
-  var edad;
-  edad = document.getElementById("edad").value;
-  if(edad<13 || edad>17) {alert("no eres un adolescente")}
-//tomo la edad
-
-
-}//FIN DE LA FUNCIÓN
