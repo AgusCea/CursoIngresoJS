8e9df94fb2e64219d53f2c7fae7c9d4874158f9d
AgusCea <52534887+AgusCea@users.noreply.github.com>
2019-08-01 12:14:56 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,18 @@
 function mostrar()
 {
-	var contador=0;
-
 	var contadorPositivos=0;
 	var contadorNegativos=0;
 	var acumuladorPositivos=0;
-	var acumuladorNegativos=0;
-	var numero;
-	
+  var acumuladorNegativos=0;
+  var contadorCeros=0;
+  var contadorPares=0;
+  var numero;
+
 	var respuesta=true;
 
 	while(respuesta==true){
 		numero=prompt("Ingrese un número: ");
-		numero=parseInt(numero);
+    numero=parseInt(numero);
 
 		if(numero>0){
 			contadorPositivos++;
@@ -20,12 +20,23 @@ function mostrar()
 		}else if(numero<0){
 			contadorNegativos++;
 			acumuladorNegativos=acumuladorNegativos+numero;
-		}
+		}else if(numero==0){
+      contadorCeros++;
+    }
+
+    if(numero%2==0){
+      contadorPares++;
+    }
 
 		respuesta=confirm("Desea escribir otro número");
-	}
-	document.write("1. suma de positivos: "+acumuladorPositivos+"<br>");
-	document.write("2. suma de negativos: "+acumuladorNegativos+"<br>");
+  }
+  document.write("1. suma de negativos: "+acumuladorNegativos+"<br>");
+	document.write("2. suma de positivos: "+acumuladorPositivos+"<br>");
 	document.write("3. cantidad de positivos: "+contadorPositivos+"<br>");
-	document.write("4. cantidad de negativos: "+contadorNegativos+"<br>");
+  document.write("4. cantidad de negativos: "+contadorNegativos+"<br>");
+  document.write("5. cantidad de ceros: "+contadorCeros+"<br>");
+  document.write("6. cantidad de numeros pares: "+contadorPares+"<br>");
+  document.write("7. promedio de positivos: "+(acumuladorPositivos/contadorPositivos)+"<br>");
+  document.write("8. promedio de negativos: "+(acumuladorNegativos/contadorNegativos)+"<br>");
+  document.write("9. diferencia entre positivos y negativos: "+(acumuladorPositivos-acumuladorNegativos)+"<br>");
 }
